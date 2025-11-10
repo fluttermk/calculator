f87f466b1c5fe923ffb16dff2b9a7c0412274116
manishkumarmk <narmalamanish4318@gmail.com>
2025-11-10 14:16:16 +0530

    initial changes

---
+++ app.js
@@ -0,0 +1,28 @@
+const outputbar = document.querySelector(".result-bar");
+const buttons = document.querySelectorAll("input[type='button']");
+buttons.forEach((btn) => btn.addEventListener("click", calculatormanger));
+
+function calculatormanger() {
+  let result = this.value;
+  if (result !== "AC" && result !== "&" && result !== "=") {
+    outputbar.value = outputbar.value + result;
+  }
+  if (result == "=") {
+    outputbar.value = eval(outputbar.value);
+  }
+
+  if (result == "AC") {
+    outputbar.value = "";
+  }
+
+  if (result == "&") {
+    let length = outputbar.value;
+    outputbar.value = length.slice(0, -1);
+  }
+}
+
+
+function peter() {
+  console.log('wekwepkwpe');
+  console.log("weber")
+}
